3ccd456c61b652ff24f5e8b507da59de0fa41a57
Vinay Sharma <vinaysharma7811@gmail.com>
2021-03-27 21:59:28 +0530

    added text

--- Hello iOS/ContentView.swift
+++ Hello iOS/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("I learnt text!")
+            .padding(15)
+            .background(Color.black)
+            .border(Color.red, width: 5)
+            .foregroundColor(Color.green)
     }
 }
 
