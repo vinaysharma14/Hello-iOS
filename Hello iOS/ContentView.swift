f86e44fbda445052018769a6661d9078d94a0f43
Vinay Sharma <vinaysharma7811@gmail.com>
2021-03-27 18:35:18 +0530

    Initial Commit

---
+++ Hello iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello iOS
+//
+//  Created by Vinay  Sharma on 27/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hello iOS/Hello_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_iOSApp.swift
+//  Hello iOS
+//
+//  Created by Vinay  Sharma on 27/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Hello_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
