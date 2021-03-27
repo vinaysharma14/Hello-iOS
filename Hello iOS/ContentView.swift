ef7771f69f359340f76019dec5cace43c84f666b
Vinay Sharma <vinaysharma7811@gmail.com>
2021-03-27 23:15:34 +0530

    added vStack, hStack, spacer and image

--- Hello iOS/ContentView.swift
+++ Hello iOS/ContentView.swift
@@ -9,11 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("I learnt text!")
-            .padding(15)
-            .background(Color.black)
-            .border(Color.red, width: 5)
-            .foregroundColor(Color.green)
+        VStack() { // ~ flex direction: column
+            Spacer() // ~ flex: 1
+            
+            Image("Steve Jobs")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200)
+            
+            // VStack(spacing: 20) could be used as well
+            // for custom spacing b/w texts instead of a spacer
+            Spacer()
+            
+            HStack { // ~ flex direction: row
+                Text("Think")
+                    .padding(15)
+                    .background(Color.black)
+                    .border(Color.blue, width: 5)
+                    .foregroundColor(Color.green)
+                
+                // HStack(spacing: 20) could be used as well
+                // for custom spacing b/w texts instead of a spacer
+                Spacer()
+                
+                Text("Different")
+                    .padding(15)
+                    .background(Color.black)
+                    .border(Color.blue, width: 5)
+                    .foregroundColor(Color.green)
+            }
+            .padding(.horizontal, 60)
+            .background(Color.green)
+            
+            Spacer()
+        }.background(Color.red)
     }
 }
 
